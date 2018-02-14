726828d50d005f6a54140332252a3578d190ddfd
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-13 22:20:58 -0500

    added some stuff

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
+      count: 0,
 
     }
   }
   render() {
     return (
      <div>
+       <h1>0</h1>
        <button className='btn btn-info'>+</button>
        <button className='btn btn-info'>-</button>
+       <input type="number"/>
      </div>
     );
   }
