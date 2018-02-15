966475e6d320a96f3506bb37ba0ccd6cd1a2f445
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-14 21:00:51 -0500

    recreated react-counter without the email and sign in form

--- src/App.js
+++ src/App.js
@@ -5,16 +5,49 @@ class App extends Component {
     super()
     this.state = {
       count: 0,
+      value: 1
 
     }
+    this.increase = this.increase.bind(this);
+
+    this.handleValueChange = this.handleValueChange.bind(this);
+
+    this.decrease = this.decrease.bind(this);
+
+    this.reset = this.reset.bind(this);
+
   }
+increase(){
+  this.setState({
+    count: this.state.count + this.state.value
+  });
+}
+decrease(){
+  this.setState({
+    count: this.state.count - this.state.value
+  });
+}
+reset(){
+  this.setState({
+    count: this.state.count = 0
+  });
+}
+handleValueChange(e){
+  this.setState({
+    value: +e.target.value
+  });
+}
+
+
+
   render() {
     return (
-     <div>
-       <h1>0</h1>
-       <button className='btn btn-info'>+</button>
-       <button className='btn btn-info'>-</button>
-       <input type="number"/>
+     <div className='container bg-color'>
+       <h1 className='h1-color'>{this.state.count}</h1>
+       <button className="btn-primary" onClick={this.reset}>RESET</button>
+       <button className='btn btn-info' onClick={this.increase}>+</button>
+       <button className='btn btn-info' onClick={this.decrease}>-</button>
+       <input type="number" className='form-control' onChange={this.handleValueChange} value={this.state.value}/>
      </div>
     );
   }
